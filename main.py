b712ab791ac5c185048444de2c33666647a9dd06
Sergio <huelva91@users.noreply.github.com>
2023-01-26 00:08:07 +0100

    Init

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)  # Avoid close chrome when script end
+chrome_options.add_argument("--start-maximized")
+
+s = Service("/home/sergio/Documentos/Proyectos/wodbusted/chromedriver/chromedriver")
+
+# Specify the path to the web drivers
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
+# Navigate to the login page
+driver.get("https://totalfitnes.wodbuster.com/account/login.aspx")
+
+email_field = driver.find_element("id", "body_body_body_body_IoEmail")
+email_field.send_keys("your_email")
+email_field = driver.find_element("id", "body_body_body_body_IoPassword")
+email_field.send_keys("your_pass")
+
+loging_button = driver.find_element("id", "body_body_body_body_CtlEntrar")
+loging_button.click()
+
+time.sleep(10)
+driver.close()
