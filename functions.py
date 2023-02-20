876a3c6d3518a54292b7303b9d8e652b944e580e
Sergio <huelva91@users.noreply.github.com>
2023-02-20 23:48:12 +0100

    Headless

--- functions.py
+++ functions.py
@@ -14,7 +14,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 def init_driver():
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)  # Avoid close chrome when script end
-    chrome_options.add_argument("--start-maximized")
+    #chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+
+
 
     # s = Service("./chromedriver/chromedriver.exe")
     # ChromeDriver Linux
@@ -35,6 +38,8 @@ def login(driver, email, password):
     loging_button = driver.find_element(By.ID, "body_body_body_body_CtlEntrar").click()
 
 
+
+
 def get_weekday():
     current_date = datetime.datetime.now()
     return current_date.weekday()
@@ -42,13 +47,15 @@ def get_weekday():
 
 def navigate_reservations(driver):
     driver.get("https://totalfitnes.wodbuster.com/athlete/reservas.aspx")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='calendar']/div/div[1]/a[2]")))
+
 
 
 
 def select_next_day(driver):
     button_next_day = driver.find_element(By.XPATH, "//*[@id='calendar']/div/div[1]/a[2]").click()
 
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div/div[2]/section/div[2]/div/div/div[2]/div[3]/div/div/div[16]/div/div/div/div[3]/button/span")))
+    #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div/div[2]/section/div[2]/div/div/div[2]/div[3]/div/div/div[16]/div/div/div/div[3]/button/span")))
     print("hi")
 
 
