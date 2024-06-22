c074cf84f48ef70da031ffbe159fb2c72cb48653
huelva91 <huelva91@gmail.com>
2024-06-22 21:08:10 +0200

    Update version web

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import urlparse, parse_qs
+
 
 import time
 
@@ -28,12 +30,14 @@ def init_driver():
 
 def login(driver, email, password):
     # Navigate to the login page
-    driver.get("https://totalfitnes.wodbuster.com/account/login.aspx")
+    driver.get("https://wodbuster.com/account/login.aspx?cb=drakkar")
 
-    driver.find_element(By.ID, "body_body_body_body_IoEmail").send_keys(email)
-    driver.find_element(By.ID, "body_body_body_body_IoPassword").send_keys(password)
+    driver.find_element(By.ID, "body_body_CtlLogin_IoEmail").send_keys(email)
+    driver.find_element(By.ID, "body_body_CtlLogin_IoPassword").send_keys(password)
 
-    driver.find_element(By.ID, "body_body_body_body_CtlEntrar").click()
+    driver.find_element(By.ID, "body_body_CtlLogin_CtlAceptar").click()
+    time.sleep(2)
+    driver.find_element(By.ID, "body_body_CtlConfiar_CtlSeguro").click()
 
 
 def get_weekday():
@@ -42,11 +46,39 @@ def get_weekday():
 
 
 def navigate_reservations(driver):
-    driver.get("https://totalfitnes.wodbuster.com/athlete/reservas.aspx")
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='calendar']/div/div[1]/a[2]")))
+    time.sleep(1)
+    driver.get("https://drakkar.wodbuster.com/athlete/reservas.aspx")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='calendar']/div/div[1]/a[3]")))
+    #driver.find_element(By.XPATH, "//*[@id='calendar']/div/div[1]/a[3]").click()
+    #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='calendar']/div/div[1]/a[2]")))
+    print (current_url(driver))
+    print("dia de la semana" + str(get_weekday()))
+    driver.get(current_url(driver))
+    boton = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/section/div[2]/div/div/div[2]/div[4]/div/div/div[17]/div/div/div/div[3]/button")
+    print(boton)
+
+
+
+def current_url(driver):
+    current_url = driver.current_url
+    # URL proporcionada
+
+    # Analiza la URL
+    parsed_url = urlparse(current_url)
+
+    # Obtiene los parámetros de consulta como un diccionario
+    query_params = parse_qs(parsed_url.query)
+
+    # Extrae el valor del parámetro 't'
+    numero = query_params.get('t', [None])[0]
+
+    numero2 = int(numero)
+    url_final = f"https://drakkar.wodbuster.com/athlete/reservas.aspx?t={numero2}"
+    return url_final
 
 
 def select_next_day(driver):
+    #numero magico 86400
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='calendar']/div/div[1]/a[2]")))
     driver.find_element(By.XPATH, "//*[@id='calendar']/div/div[1]/a[2]").click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='calendar']/div/div[1]/a[2]")))
@@ -57,7 +89,24 @@ def select_next_day(driver):
 
 
 def select_class(driver):
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='calendar']/div/div[16]/div/div/div/div[3]/button")))
+    print("hola")
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='calendar']/div/div[16]/div/div/div/div[3]/button")))
+    current_url = driver.current_url()
+    # URL proporcionada
+
+    # Analiza la URL
+    parsed_url = urlparse(current_url)
+
+    # Obtiene los parámetros de consulta como un diccionario
+    query_params = parse_qs(parsed_url.query)
+
+    # Extrae el valor del parámetro 't'
+    numero = query_params.get('t', [None])[0]
+
+
+    print(numero)
+
 
     driver.find_element(By.XPATH,
                         "//*[@id='calendar']/div/div[16]/div/div/div/div[3]/button").click()
--- main.py
+++ main.py
@@ -12,8 +12,8 @@ driver = init_driver()
 
 login(driver, email, password)
 navigate_reservations(driver)
-select_next_day(driver)
-schedule.every().day.at("23:06").do(select_class(driver))
+#select_next_day(driver)
+#schedule.every().day.at("23:06").do(select_class(driver))
 
 time.sleep(10)
 close_conecction(driver)
